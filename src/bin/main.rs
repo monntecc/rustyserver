a40bd9b9aa65761ec981b814d8810c46482ab86f
Potapenko <nylestroke@gmail.com>
2024-04-28 16:08:29 +0200

    Implement drop trait for a thread pool, refactor sender structure

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,65 +9,71 @@ static IP_ADDRESS: &'static str = "127.0.0.1";
 static PORT: &'static str = "8080";
 
 fn main() {
+    // Bind to a specific IP address and port to listen for incoming connections
     let address = format!("{}:{}", IP_ADDRESS, PORT);
-
-    // Create tcp listener
     let listener = TcpListener::bind(address).unwrap();
 
-    // Create thread pool
-    let pool = ThreadPool::new(4);
+    // Create a thread pool to handle concurrent connections efficiently
+    let pool = ThreadPool::new(4); // Create a pool with 4 threads
 
-    // Loop through all connections
+    // Continuously accept and handle new connections
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = stream.unwrap(); // Wait for the next incoming connection
 
+        // Move ownership of the stream to the closure for handling within the thread pool
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
-// Response from web server must be an following format:
-/*
-    HTTP-VERSION Status-Code Reason-Phrase CRLF
-    headers CLRF
-    message-body
-
-    e.g: HTTP/1.1 200 OK\r\n\r\n
-*/
+// Response from web server must follow a specific format:
+//  * Status line (e.g., HTTP/1.1 200 OK)
+//  * Headers (e.g., Content-Length)
+//  * Empty line
+//  * Message body (optional)
 fn handle_connection(mut stream: TcpStream) {
-    // Create buffer to copy data from stream here
+    // Buffer to hold received data from the connection
     let mut buffer = [0; 1024];
-    // Read the stream anc copy data to buffer
+
+    // Read data from the stream into the buffer
     stream.read(&mut buffer).unwrap();
 
-    // Print to console web server request
-    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // Print the received request for debugging purposes (optional)
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    // Hardcoded get request header
+    // Define byte sequences to identify different request types
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
+    // Determine the appropriate response based on the request type
     let (status_line, filename) = if buffer.starts_with(get) {
-        // Return index page
+        // Serve the index.html page for a regular GET request
         ("HTTP/1.1 200 OK", "public/index.html")
     } else if buffer.starts_with(sleep) {
-        // Return index page, but waiting 5 seconds
+        // Simulate a delay of 5 seconds for the sleep request
         thread::sleep(Duration::from_secs(5));
+        // Then serve the index.html page
         ("HTTP/1.1 200 OK", "public/index.html")
     } else {
-        // Return 404 page
+        // Return a 404 Not Found response for unrecognized requests
         ("HTTP/1.1 404 NOT FOUND", "public/404.html")
     };
 
-    // Return a response
+    // Read the requested file content
     let contents = fs::read_to_string(filename).unwrap();
+
+    // Format the response following the HTTP protocol
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
         contents.len(),
         contents
     );
+
+    // Send the response back to the client
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 // Library crate
-
 use std::{
     sync::{mpsc, Arc, Mutex}, // Import necessary synchronization primitives from the standard library
     thread,                   // Import threading functionalities
@@ -7,11 +6,22 @@ use std::{
 
 #[allow(dead_code)]
 pub struct ThreadPool {
-    workers: Vec<Worker>,      // Vector to store the worker threads
-    sender: mpsc::Sender<Job>, // Sender channel to send jobs to workers
+    // Vector to store the worker threads
+    workers: Vec<Worker>,
+    // Sender channel to send jobs to workers
+    sender: mpsc::Sender<Message>,
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>; // Define the type of job - a boxed function pointer
+// Define the type of job - a boxed function pointer with ownership semantics
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+#[allow(dead_code)]
+enum Message {
+    // Message to signal a new job for the worker
+    NewJob(Job),
+    // Message to signal termination for the worker
+    Terminate,
+}
 
 // Implement the ThreadPool
 impl ThreadPool {
@@ -31,7 +41,8 @@ impl ThreadPool {
         let mut workers = Vec::with_capacity(size); // Initialize an empty worker vector with capacity
 
         for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver))); // Add worker threads to the pool
+            // Add worker threads to the pool with a reference to the shared receiver channel
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         Self { workers, sender } // Return the ThreadPool instance
@@ -42,25 +53,58 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static, // Specify the closure type for the job
     {
         let job = Box::new(f); // Box the closure to transfer ownership
-        self.sender.send(job).unwrap(); // Send the job over the channel
+        self.sender.send(Message::NewJob(job)).unwrap(); // Send the job over the channel
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers!");
+
+        for _ in &self.workers {
+            // Send termination message to all workers
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker: {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                // Join the worker thread to wait for completion
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 #[allow(dead_code)]
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>, // Handle to the worker thread
+    // Handle to the worker thread, initially holding the join handle
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            // Get a job from the channel
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing..", id);
-            job(); // Execute the received job
+            // Get a job from the channel (receiver) with locking for thread safety
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing..", id);
+                    job(); // Execute the received job
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break; // Exit the loop on termination message
+                }
+            }
         });
 
-        Self { id, thread }
+        Self {
+            id,
+            thread: Some(thread),
+        }
     }
 }
