c1313f73f38e405cd1fff5a57661225c53b0ff44
Potapenko <nylestroke@gmail.com>
2024-04-28 15:27:14 +0200

    Create tcp listener, handle request/response, add index and 404 pages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+static IP_ADDRESS: &'static str = "127.0.0.1";
+static PORT: &'static str = "8080";
+
 fn main() {
-    println!("Hello, world!");
+    let address = format!("{}:{}", IP_ADDRESS, PORT);
+
+    // Create tcp listener
+    let listener = TcpListener::bind(address).unwrap();
+
+    // Loop through all connections
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+// Response from web server must be an following format:
+/*
+    HTTP-VERSION Status-Code Reason-Phrase CRLF
+    headers CLRF
+    message-body
+
+    e.g: HTTP/1.1 200 OK\r\n\r\n
+*/
+fn handle_connection(mut stream: TcpStream) {
+    // Create buffer to copy data from stream here
+    let mut buffer = [0; 1024];
+    // Read the stream anc copy data to buffer
+    stream.read(&mut buffer).unwrap();
+
+    // Print to console web server request
+    //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // Hardcoded get request header
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        // Return index page
+        ("HTTP/1.1 200 OK", "public/index.html")
+    } else {
+        // Return 404 page
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
+
+    // Return a response
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
